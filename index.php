06bcb5c58a1793d5c3d998d78afc01148812f93b
mivlev1 <makzoyn@gmail.com>
2022-03-09 10:52:34 +0300

    edit2

--- index.php
+++ index.php
@@ -25,3 +25,28 @@ print($v1);
 
 // Если надо выполнить код из другого файла.
 // include('form.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>second-task</title>
+</head>
+<body>
+    <h1>Connect</h1>
+    <img src="/screenshots/putty-connect.png" alt="putty-connect">
+    <h1>HTTP 1.0</h1>
+    <img src="/screenshots/HTTP-1.0.png" alt="http-1.0">
+    <h1>HTTP 1.1</h1>
+    <img src="/screenshots/HTTP-1.1.png" alt="http-1.1">
+    <h1>Размер file.tar.gz</h1>
+    <img src="/screenshots/file.tar.png" alt="file.tar">
+    <h1>Медиатип image.png</h1>
+    <img src="/screenshots/image.png" alt="image">
+    <h1>Получаем первые 200 байт file.tar</h1>
+    <img src="/screenshots/200BYTES.png" alt="100byte">
+    <h1>Определем кодировку index.php</h1>
+    <img src="/screenshots/code.png" alt="code-index.php">
+</body>
+</html>
