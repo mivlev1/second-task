7a45240f37c6ebd959437e3e612cf7970eadd8e8
mivlev1 <makzoyn@gmail.com>
2022-03-09 13:54:40 +0300

    editc

--- index.php
+++ index.php
@@ -25,13 +25,11 @@ print($v1); <
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>second-task</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>second-task</title>
 </head>
 <body>
     <h1>Connect</h1>
